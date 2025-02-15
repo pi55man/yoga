4cdd49fdc205a5a746b55072ef3bb5f1f1c0a54d
nandinitiwari827 <nandini.tiwari1@s.amity.edu>
2025-02-15 14:59:14 +0530

    nandini

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from '../components/n components/Header/Header'
+import Footer from '../components/n components/Footer/Footer'
 
 const Home = () => {
   return (
