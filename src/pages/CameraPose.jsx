f769e955249de8faddda30d59b36428228885128
naitik328 <naitiktiwari2006@gmail.com>
2025-02-15 14:43:03 +0530

    staring

---
+++ src/pages/CameraPose.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CameraPose = () => {
+  return (
+    <div>CameraPose</div>
+  )
+}
+
+export default CameraPose
---
+++ src/pages/YogaPoses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const YogaPoses = () => {
+  return (
+    <div>YogaPoses</div>
+  )
+}
+
+export default YogaPoses
