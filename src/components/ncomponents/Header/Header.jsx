ed4c6112f3848f7cf252e53f8f362c1e15c5bd44
nandinitiwari827 <nandini.tiwari1@s.amity.edu>
2025-02-15 16:18:24 +0530

    home

---
+++ src/components/ncomponents/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-pink-600'>
+nandni
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,98 @@
 import React from 'react'
-import Header from '../components/n components/Header/Header'
-import Footer from '../components/n components/Footer/Footer'
+import { Link, NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className='shadow sticky top-0 z-50'>
+        <nav className='bg-white'>
+          <div className='flex justify-between items-center h-16 px-4 lg:px-8'>
+            <Link to="/" className="flex items-center">
+              <img 
+                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                className="h-12"
+                alt="Logo"
+              />
+            </Link>
+            <div className="flex items lg:order-2">
+              <Link
+                to="#"
+                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              >
+                Log in
+              </Link>
+              <Link
+                to="#"
+                className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              >
+                Get started
+              </Link>
+            </div>
+            <div className="hidden justify-between items-center w-full pl-[550px] lg:flex lg:w-auto lg:order-1"
+              id="mobile-menu-2"
+            >
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                  <NavLink
+                    to=""
+                    className={({ isActive }) =>
+                      `block py-2 pr-4 pl-3 duration-200 ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                      `block py-2 pr-4 pl-3 duration-200 ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/contact"
+                    className={({ isActive }) =>
+                      `block py-2 pr-4 pl-3 duration-200 ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/github"
+                    className={({ isActive }) =>
+                      `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    Github
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className="text-8xl text-black font-semibold w-[600px] mt-5 ml-6">Training & fitness Health</div>
+    <Link to="/" className="ml-60">
+              <img 
+                src="https://i.pinimg.com/474x/05/22/dd/0522ddd6e4622c0a3779c4c44578d382.jpg"
+                className="h-72"
+                alt="Logo"
+              />
+            </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
